e2cbcccc8fab3da9637f3cdc641fce10f7e46b4a
CristianDavidFelix <david.df.01@hotmail.com>
2023-01-26 21:52:14 -0500

    tablero V02

--- Tercer Parcial/Tabla de ajedrez/main.cpp
+++ Tercer Parcial/Tabla de ajedrez/main.cpp
@@ -33,3 +33,38 @@ int main()
     {
         cout << "Error al abrir el archivo" << endl;
         return 1;
+    }
+
+    // Contador de filas
+    int filas = 0;
+    // Recorre cada línea del archivo
+    string line;
+    while (getline(file, line))
+    {
+        filas++;
+        // Verifica que la longitud de la línea sea 8
+        if (line.length() != N)
+        {
+            cout << "Error: El archivo debe tener 8 columnas en cada fila" << endl;
+            return 1;
+        }
+        // Verifica que cada caracter de la línea sea 0 o 1
+        for (int i = 0; i < line.length(); i++)
+        {
+            if (line[i] != '0' && line[i] != '1')
+            {
+                cout << "Error: El archivo debe tener solo 0 y 1" << endl;
+                return 1;
+            }
+        }
+    }
+    // Verifica que el archivo tenga 8 filas
+    if (filas != N)
+    {
+        cout << "Error: El archivo debe tener 8 filas" << endl;
+        return 1;
+    }
+    file.close();
+    // Esta parte es para centrar el tablero en la pantalla
+    // Obtiene el ancho de la consola
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
